b15504a5d38ea587ad97195e502bf98d5d11d96b
hosseingodly <38300372+hosseingodly@users.noreply.github.com>
2018-04-12 13:46:59 +0430

    Update mybot1.php

--- mybot1.php
+++ mybot1.php
@@ -1,6 +1,6 @@
 ﻿include("Telegram.php");
 date_default_timezone_set("asia/tehran");
-// Set the bot 555177137:AAEJu_mOjCPq8BJbtQbULQMmN_SOjUrjQFI
+// Set the bot TOKEN
 $bot_id = "555177137:AAEJu_mOjCPq8BJbtQbULQMmN_SOjUrjQFI";
 // Instances the class
 $telegram = new Telegram($bot_id);
